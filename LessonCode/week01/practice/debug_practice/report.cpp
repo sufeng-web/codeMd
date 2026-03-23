507da36d72f4f756148d46b2ffcff4dc59ffd092
liujinxin <2546141496@qq.com>
2026-03-23 19:06:27 +0800

    [Fix] fix coding bug

--- LessonCode/week01/practice/debug_practice/report.cpp
+++ LessonCode/week01/practice/debug_practice/report.cpp
@@ -7,9 +7,9 @@ void Report::print(const AnalysisResult& result) const {
     std::cout << std::fixed << std::setprecision(2);
     std::cout << RESULTS_HEADER;
     std::cout << "Student: " << g_studentName << "\n";
-    std::cout << LABEL_TOTAL << result.Total << "\n";
-    std::cout << LABEL_TOTAL << result.Average << "\n";
-    std::cout << LABEL_PASS_COUNT << result.PassCount << "\n";
-    std::cout << LABEL_MAX << result.MaxScore << "\n";
+    std::cout << LABEL_TOTAL << result.total << "\n";
+    std::cout << LABEL_TOTAL << result.average << "\n";
+    std::cout << LABEL_PASS_COUNT << result.passCount << "\n";
+    std::cout << LABEL_MAX << result.maxScore << "\n";
     std::cout << "Courses: " << g_courseCount << "\n";
 }
