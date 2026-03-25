e2a229cf97f42299260fc0162c3183a1338113a4
liujinxin <2546141496@qq.com>
2026-03-25 15:47:14 +0800

    [Fix] 修改build.bat 限制模型启动

--- LessonCode/week01/homework/build.bat
+++ LessonCode/week01/homework/build.bat
@@ -4,4 +4,4 @@ cd build
 cmake .. -G "Visual Studio 17 2022" -A x64
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 ren build\bin\Release\llama-cli.exe localllm.exe
-build\bin\Release\localllm.exe -m models\qwen2.5-1.5b-instruct-q4_0.gguf 
+#build\bin\Release\localllm.exe -m models\qwen2.5-1.5b-instruct-q4_0.gguf 
