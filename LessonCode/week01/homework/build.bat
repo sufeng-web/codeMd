248837b3df8341a0d8646dca39c9db229b65705b
liujinxin <2546141496@qq.com>
2026-03-25 16:25:50 +0800

    [Fix] 修改重命名路径

--- LessonCode/week01/homework/build.bat
+++ LessonCode/week01/homework/build.bat
@@ -3,7 +3,7 @@ mkdir build
 cd build
 cmake .. -G "Visual Studio 17 2022" -A x64
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-ren build\bin\Release\llama-cli.exe localllm.exe
+ren bin\Release\llama-cli.exe localllm.exe
 #启动模型，把模型放入相应的位置即可，取消注释运行模型
 #build\bin\Release\localllm.exe -m models\qwen2.5-1.5b-instruct-q4_0.gguf  
 pause
