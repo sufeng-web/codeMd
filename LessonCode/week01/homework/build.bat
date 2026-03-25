4b08bab25be0331580b7250ee2cfe521bd11bf01
liujinxin <2546141496@qq.com>
2026-03-25 16:32:09 +0800

    [Fix] 模型启动路径修改

--- LessonCode/week01/homework/build.bat
+++ LessonCode/week01/homework/build.bat
@@ -5,5 +5,5 @@ cmake .. -G "Visual Studio 17 2022" -A x64
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 ren bin\Release\llama-cli.exe localllm.exe
 #启动模型，把模型放入相应的位置即可，取消注释运行模型
-#build\bin\Release\localllm.exe -m models\qwen2.5-1.5b-instruct-q4_0.gguf  
+#bin\Release\localllm.exe -m models\qwen2.5-1.5b-instruct-q4_0.gguf  
 pause
